394e3a1e97848e031b7a9107ecd832c2918d5bdf
mappidi <mappidi@novelirs.com>
2023-04-30 16:33:47 +0530

    Base Script

---
+++ get_awc-vpc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+REGION='us-east-1'
+FORMAT='table'
+aws ec2 describe-vpcs --region $REGION --output $FORMAT
---
+++ script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+A=10
+B=20
+C='Welcome to Devops Training In Telugu'
+
+
+echo $A
+echo ${B}
+echo "${C}"
