91182c478cbef4027ba488cec9ba1310595adee3
mappidi <mappidi@novelirs.com>
2023-05-05 17:11:51 +0530

    new file added with $@

---
+++ effective_get_vpc.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+REGIONS=$@
+echo ${REGIONS}
+for REGION in ${REGIONS}
+    do aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].VpcId"
+    echo '---------------------------------------------------------------------'
+done
