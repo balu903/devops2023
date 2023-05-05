439d1dee76ae99b21514b6de01c15cf5c9105016
mappidi <mappidi@novelirs.com>
2023-05-05 17:25:46 +0530

    updated the file

--- effective_get_vpc.sh
+++ effective_get_vpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGIONS=$@
 echo ${REGIONS}
-$FORMAT='json'
+FORMAT='json'
 for REGION in ${REGIONS};do
     echo " Get the VPC ID for the Region ${REGION}"
     aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].VpcId"
