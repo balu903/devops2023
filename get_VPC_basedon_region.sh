aff20e59f5f2ce3570755e07409ff5ff98378b88
mappidi <mappidi@novelirs.com>
2023-05-05 18:21:24 +0530

    updated set -e

--- get_VPC_basedon_region.sh
+++ get_VPC_basedon_region.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-set -x
+set -x -e
 echo "1"
 echo "2"
 echo "3"
 1
 2
 3
-
 REGION=$1
 echo "Lets get the VPC Details from the Region $REGION"
 aws ec2 describe-vpcs --region $REGION --output json | jq ".Vpcs[].VpcId"
