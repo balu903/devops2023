be6924ecbb3d4b45defa8227d58be16ab7cdf872
mappidi <mappidi@novelirs.com>
2023-05-05 12:16:45 +0530

    modified:   get_awc-vpc.sh

--- get_awc-vpc.sh
+++ get_awc-vpc.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-REGION='us-east-1'
-FORMAT='table'
-aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].VpcID
+#REGION='us-east-1'
+echo "Getting the VPC ID for region $1"
+REGION=$1
+FORMAT='json'
+#FORMAT=$1
+echo "Lets Get VPC details from the Regio $REGION"
+aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].VpcId"
+echo '---------------------------------------------------------------------'
+
+echo "Getting the VPC ID for region $2"
+REGION=$2
+FORMAT='json'
+echo "Lets Get VPC details from the Regio $REGION"
+aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].VpcId"
+
