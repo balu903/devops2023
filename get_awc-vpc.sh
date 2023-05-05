e06de43814fdeccab8ae873ea395d0ba025e17f9
mappidi <mappidi@novelirs.com>
2023-05-05 12:20:11 +0530

    Added  as a new region

--- get_awc-vpc.sh
+++ get_awc-vpc.sh
@@ -10,6 +10,7 @@ echo '---------------------------------------------------------------------'
 
 echo "Getting the VPC ID for region $2"
 REGION=$2
+echo "adding 2nd region"
 FORMAT='json'
 echo "Lets Get VPC details from the Regio $REGION"
 aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].VpcId"
