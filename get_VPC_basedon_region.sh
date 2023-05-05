04b78593788e83528b85d0603cca9688fe0f05ee
mappidi <mappidi@novelirs.com>
2023-05-05 18:31:11 +0530

    updated set -e

--- get_VPC_basedon_region.sh
+++ get_VPC_basedon_region.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -x -e
-echo "1"
-echo "2"
-echo "3"
-1
-2
-3
 REGION=$1
+if [ $# -gt 0]
+then
 echo "Lets get the VPC Details from the Region $REGION"
 aws ec2 describe-vpcs --region $REGION --output json | jq ".Vpcs[].VpcId"
+else
+echo " You have given $# Params means no arguments given"
+fi
