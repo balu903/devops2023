60d172d58aa94aa8b4a60c9fda1faa40f7ac179a
mappidi <mappidi@novelirs.com>
2023-05-05 18:38:12 +0530

    updated set -e

--- effective_get_vpc.sh
+++ effective_get_vpc.sh
@@ -2,6 +2,7 @@
 REGIONS=$@
 echo ${REGIONS}
 FORMAT='json'
+VERSION='1.0.0'
 for REGION in ${REGIONS};do
     echo " Get the VPC ID for the Region ${REGION}"
     aws ec2 describe-vpcs --region $REGION --output $FORMAT | jq ".Vpcs[].VpcId"
--- get_VPC_basedon_region.sh
+++ get_VPC_basedon_region.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -x -e
-REGION=$1
-if [ $# -gt 0 ]
+REGION1=$1
+REGION2=$2
+REGION3=$3
+if [ $# -gt 0]
 then
-echo "Lets get the VPC Details from the Region $REGION"
-aws ec2 describe-vpcs --region $REGION --output json | jq ".Vpcs[].VpcId"
+echo "Lets get the VPC Details from the Region $REGION1"
+aws ec2 describe-vpcs --region $REGION1 --output json | jq ".Vpcs[].VpcId"
+echo "Lets get the VPC Details from the Region $REGION2"
+aws ec2 describe-vpcs --region $REGION2 --output json | jq ".Vpcs[].VpcId"
+echo "Lets get the VPC Details from the Region $REGION3"
+aws ec2 describe-vpcs --region $REGION3 --output json | jq ".Vpcs[].VpcId"
 else
 echo " You have given $# Params means no arguments given"
 fi
