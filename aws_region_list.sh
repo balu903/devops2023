21555fc796f17cae6db6c6a55e5855893a5e8cde
mappidi <mappidi@novelirs.com>
2023-05-06 22:07:10 +0530

    updated set -e

--- aws_region_list.sh
+++ aws_region_list.sh
@@ -4,15 +4,22 @@
 # 3. If Valid get the VPC ID of the region
 # 4. If not throw error as invalid name.
 
-REGIONS=('us-east-1' 'us-east-2' 'ind-hyd-1' 'ap-south-1' 'eu-west-1')
+# REGIONS=('us-east-1' 'us-east-2' 'ind-hyd-1' 'ap-south-1' 'eu-west-1')
 
-#to print the list elements 
-echo ${RGIONS[@]}
-#to print  lenght of the list  
-echo ${#RGIONS[@]}
-# to iterate
-for REGION in ${REGIONS[@]};do
-    echo $REGIO
+# #to print the list elements
+# echo ${RGIONS[@]}
+# #to print  lenght of the list
+# echo ${#RGIONS[@]}
+# # to iterate
+# for REGION in ${REGIONS[@]};do
+#     echo $REGIO
+# done
+# # to print rane of elements
+# echo ${#RGIONS[@]:0:4}
+
+#!/bin/bash
+REGIONS=('us-east-1' 'us-east-2' 'ind-hyd-1' 'ap-south-1' 'eu-west-1')
+for REGION in ${REGIONS[@]}; do
+    echo "The REGION name is $REGION"
+    echo "---------------------------"
 done
-# to print rane of elements 
-echo ${#RGIONS[@]:0:4}
