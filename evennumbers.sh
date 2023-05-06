0a823a0c9c38a6f8d5b2ae8c79c989f6b11d45bb
mappidi <mappidi@novelirs.com>
2023-05-06 21:02:13 +0530

    updated set -e

--- evennumbers.sh
+++ evennumbers.sh
@@ -11,7 +11,20 @@
 #     fi
 # done
         
-for NUM in {1..100}
+# for NUM in {1..100}
+# do
+#     if [ $(expr $NUM % 2) -eq 0 ]
+#     then
+#         echo "$NUM is a even number"
+#     else
+#         echo "$NUM is a odd number"
+#     fi
+# done
+
+
+NUMS=(10 35 67 78 96 45 34 32 12 34)
+
+for NUM in ${NUMS[@]}
 do
     if [ $(expr $NUM % 2) -eq 0 ]
     then
@@ -20,4 +33,3 @@ do
         echo "$NUM is a odd number"
     fi
 done
-        
