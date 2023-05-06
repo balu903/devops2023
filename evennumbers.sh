d4589a6f921eb2a98f58be3ceb29a6ff2147b9e2
mappidi <mappidi@novelirs.com>
2023-05-06 20:50:05 +0530

    updated set -e

---
+++ evennumbers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#NUM='100'
+while true
+    read -p "Enter the Number to Check:" NUM
+    if [ $(expr $NUM %2) -eq 0]
+    then
+        echo "$NUM is a even number"
+    else
+        echo "$NUM is a odd number"
+    fi
+done
+        
